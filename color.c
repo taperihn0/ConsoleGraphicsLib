284edd83846df25073463f98f2c7c7d68a5ee6a0
taperihn0 <szymonstar690@gmail.com>
2025-05-02 19:02:46 +0200

    8bit color palette

--- color.c
+++ color.c
@@ -3,11 +3,11 @@
 #include <limits.h>
 
 typedef struct _predef_color {
-	_color_t id;
+	_color_t code;
 	float r, g, b;
 } _predef_color;
 
-#define _PREDEF_COLOR_NUM 8
+#define _PREDEF_COLOR_NUM 256
 
 typedef struct _predef_color_list {
 	_predef_color list[_PREDEF_COLOR_NUM];
@@ -15,44 +15,80 @@ typedef struct _predef_color_list {
 
 _predef_color_list predef;
 
-void _add_color_at(size_t idx, _color_t col, short r, short g, short b) {
-	ASSERT(idx < _PREDEF_COLOR_NUM, "Invalid index while"
-"adding a new color");
-	predef.list[idx] = (_predef_color){
-		.id = col,
-		.r = r / 1000.f,
-		.g = g / 1000.f,
-		.b = b / 1000.f,
+void _add_color(_color_t code, short r, short g, short b) {
+	ASSERT(code < _PREDEF_COLOR_NUM, "Invalid color");
+	predef.list[code] = (_predef_color){
+		.code = code,
+		.r = r / 255.f,
+		.g = g / 255.f,
+		.b = b / 255.f,
 	};
 }
+
+// HIGH-INTENSITY COLORS
+#define COLOR_BRIGHT_BLACK 8
+#define COLOR_BRIGHT_RED 9
+#define COLOR_BRIGHT_GREEN 10
+#define COLOR_BRIGHT_YELLOW 11
+#define COLOR_BRIGHT_BLUE 12
+#define COLOR_BRIGHT_MAGENTA 13
+#define COLOR_BRIGHT_CYAN 14
+#define COLOR_BRIGHT_WHITE 15
 
 #define _BACKGROUND_COLOR COLOR_BLACK
 
 void _init_colors() {
 	start_color();
+	
+	// colors 0-7
+	_add_color(COLOR_BLACK, 0, 0, 0);
+	_add_color(COLOR_RED, 205, 0, 0);
+	_add_color(COLOR_GREEN, 0, 205, 0);
+	_add_color(COLOR_YELLOW, 205, 205, 0);
+	_add_color(COLOR_BLUE, 0, 0, 238);
+	_add_color(COLOR_MAGENTA, 205, 0, 205);
+	_add_color(COLOR_CYAN,0, 205, 205);
+	_add_color(COLOR_WHITE, 229, 229, 229); 
+	
+	// colors 8-15
+	_add_color(COLOR_BRIGHT_BLACK, 0, 0, 0);
+	_add_color(COLOR_BRIGHT_RED, 255, 0, 0);
+	_add_color(COLOR_BRIGHT_GREEN, 0, 255, 0);
+	_add_color(COLOR_BRIGHT_YELLOW, 255, 255, 0);
+	_add_color(COLOR_BRIGHT_BLUE, 92, 92, 255);
+	_add_color(COLOR_BRIGHT_MAGENTA, 255, 0, 255);
+	_add_color(COLOR_BRIGHT_CYAN, 0, 255, 255);
+	_add_color(COLOR_BRIGHT_WHITE, 255, 255, 255);
+	
+	// 16-231 as a 6x6x6 color cube
+     for (UINT red = 0; red < 6; red++) {
+     	for (UINT green = 0; green < 6; green++) {
+     		for (UINT blue = 0; blue < 6; blue++) {
+     			_color_t code = 16 + (red * 36) + (green * 6) + blue;
+				short r = (red ? red * 40 + 55 : 0);
+				short g = (green ? green * 40 + 55 : 0);
+				short b = (blue ? blue * 40 + 55 : 0);
+				_add_color(code, r, g, b);
+     		}
+     	}
+     }
+	
+	// 232-255 as a grayscale ramp
+	for (UINT gray = 0; gray < 24; gray++) {
+		short level = 10 * gray + 8;
+		_color_t code = 232 + gray;
+		_add_color(code, level, level, level);
+	}
 
-	_add_color_at(0, COLOR_BLACK, 0, 0, 0);
-	_add_color_at(1, COLOR_RED, 1000, 0, 0);
-	_add_color_at(2, COLOR_GREEN, 0, 1000, 0);
-	_add_color_at(3,	COLOR_YELLOW, 1000, 1000, 0);
-	_add_color_at(4, COLOR_BLUE, 0, 0, 1000);
-	_add_color_at(5, COLOR_MAGENTA, 1000, 0, 1000);
-	_add_color_at(6, COLOR_CYAN,0, 1000, 1000);
-	_add_color_at(7, COLOR_WHITE, 1000, 1000, 1000);
-
-	init_pair(1, COLOR_BLACK, _BACKGROUND_COLOR);
-	init_pair(2, COLOR_RED, _BACKGROUND_COLOR);
-	init_pair(3, COLOR_GREEN, _BACKGROUND_COLOR);
-	init_pair(4,	COLOR_YELLOW, _BACKGROUND_COLOR);
-	init_pair(5, COLOR_BLUE, _BACKGROUND_COLOR);
-	init_pair(6, COLOR_MAGENTA, _BACKGROUND_COLOR);
-	init_pair(7, COLOR_CYAN, _BACKGROUND_COLOR);
-	init_pair(8, COLOR_WHITE, _BACKGROUND_COLOR);
+	// initialize 256 pairs of colors with a default background coloring
+	for (UINT col = 0; col < _PREDEF_COLOR_NUM; col++) {
+		init_pair(col + 1, col, _BACKGROUND_COLOR);
+	}
 }
 
-_color_t _color_by_rgb(float r, float g, float b) {
+_ncurses_pair_id _color_by_rgb(float r, float g, float b) {
 	float min_diff = 10.f;
-	_color_t best = 0;
+	_color_t code = 0;
 
 	for (UINT i = 0; i < _PREDEF_COLOR_NUM; i++) {
 		float diff = fabs(predef.list[i].r - r) 
@@ -61,9 +97,10 @@ _color_t _color_by_rgb(float r, float g, float b) {
 
 		if (diff < min_diff) {
 			min_diff = diff;
-			best = predef.list[i].id;
+			code = i;
 		}
 	}
-
-	return best + 1;
+	
+	// pair number is shifted by one (0 is not used, we are numering from 1)
+	return code + 1;
 }
--- color.h
+++ color.h
@@ -7,7 +7,7 @@ typedef short _color_t;
 
 void _init_colors();
 
-_color_t _color_by_rgb(float r, float g, float b);
+_ncurses_pair_id _color_by_rgb(float r, float g, float b);
 
 _STATIC _FORCE_INLINE _ncurses_pair_id color_pair_id(float r, float g, float b)  {
 	return _color_by_rgb(r, g, b);
